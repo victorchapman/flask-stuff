04f5d07ff4caca818ff2546615959b7cd24bbe91
Victor C <victor.f.chapman@gmail.com>
2021-07-19 15:41:24 +0200

    trying get requests

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
+import requests, os
 
 app = Flask(__name__)
 
@@ -9,9 +10,16 @@ def hello_world():
     return render_template("home.html")
 
 
-@app.route("/repositories")
-def hello():
-    return render_template("repositories.html")
+@app.route("/repositories", methods=["GET"])
+def repositories():
+    repos = request.json
+    a_value = repos["https://disease.sh/v3/covid-19/countries/usa"]
+    return render_template("repositories.html", a_value=a_value)
+
+
+@app.route("/images")
+def images():
+    return render_template("images.html")
 
 
 @app.route("/user/<string:username>")
